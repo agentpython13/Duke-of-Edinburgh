3625238e8362caa2d509f9a497b84837886b7cfb
agentpython13 <153403315+agentpython13@users.noreply.github.com>
2025-03-25 18:29:09 +0000

    Delete Season Jumping.py

--- Season Jumping.py
+++
@@ -1,9 +0,0 @@
-seasons = ["spring","summer","autumn","winter"]
-season = input()
-num = int(input())
-index = seasons.index(season)
-for repeat in range(num):
-    if index > 3:
-        index = 0
-    index += 1
-print(seasons[index])
